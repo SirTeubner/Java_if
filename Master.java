ef474911c9292366235abd68cd25e668e57ab2c5
Noah Teubner <nteubner@schueler.hakzell.at>
2023-10-29 01:18:16 +0200

    initial commit

---
+++ Master.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class Master{
+    public static void main(String[] args) throws IOException{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        System.out.println("Geben Sie die Zahl 3 ein:");
+        String eingabe = br.readLine();
+        int zahl = Integer.parseInt (eingabe);
+
+        if (zahl == 3){
+            System.out.println("Die Eingabe ist richtig!");
+        }
+        
+
+        
+        
+        /*boolean bedingung = true;
+
+        if (bedingung){
+            System.out.println("Bedigung 1 erfüllt.");
+        }
+
+        bedingung = false;
+        if(bedingung){
+            System.out.println("Bedingung 2 erfüllt.");
+        }
+        */
+    }
+}
